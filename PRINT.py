4fe11c96c0e7c41ae914fa7e87e6043f730b0d9b
imarjunpagar <pagararjun52@gmail.com>
2026-01-06 20:25:29 +0530

    First commit of PRINT.py

---
+++ PRINT.py
@@ -0,0 +1,31 @@
+# # 1 - Jan - 2026
+def main():
+# #simple 1st printing programs
+ print("This Comeback is Real")
+
+print("Hello Worrld")
+
+
+print("I will conquer and Be the king")
+
+
+#variables
+
+# print name in different ways
+
+Name = "Arjun"
+print(Name)
+
+
+name = input("What is your Name ?")
+print(name)
+ 
+Id = input("Enter Your name:")
+print("hello " + Id)
+
+abc = input("Enter name:")
+print(f"hello, {abc}")
+
+
+
+main()
